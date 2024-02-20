8d1d71848ca28db030bcf95eacf1cbc784243fa1
Nouman <noumankhan95@yahoo.com>
2024-02-21 01:27:32 +0500

    settings page bug fixed

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -303,6 +303,44 @@ const Settings = () => {
             </div>
           </div>
         ))}
+        <div className="w-full relative -ml-5 my-8">
+          <svg
+            viewBox="0 0 1024 1024"
+            height={30}
+            width={30}
+            version="1.1"
+            className="icon absolute right-0 cursor-pointer"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#000000"
+            onClick={() => {
+              formikobj.setFieldValue('details', [
+                ...formikobj.values.details,
+                { from: '', to: '', price: 0 },
+              ]);
+            }}
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M512 1024C229.7 1024 0 794.3 0 512S229.7 0 512 0s512 229.7 512 512-229.7 512-512 512z m0-938.7C276.7 85.3 85.3 276.7 85.3 512S276.7 938.7 512 938.7 938.7 747.3 938.7 512 747.3 85.3 512 85.3z"
+                fill="#00ff2a"
+              ></path>
+              <path
+                d="M682.7 554.7H341.3c-23.6 0-42.7-19.1-42.7-42.7s19.1-42.7 42.7-42.7h341.3c23.6 0 42.7 19.1 42.7 42.7s-19.1 42.7-42.6 42.7z"
+                fill="#00ff1e"
+              ></path>
+              <path
+                d="M512 725.3c-23.6 0-42.7-19.1-42.7-42.7V341.3c0-23.6 19.1-42.7 42.7-42.7s42.7 19.1 42.7 42.7v341.3c0 23.6-19.1 42.7-42.7 42.7z"
+                fill="#00ff1e"
+              ></path>
+            </g>
+          </svg>
+        </div>
         <div className="w-full md:w-2/5">
           <label className="mb-3 block text-black dark:text-white">
             App Price
@@ -401,44 +439,7 @@ const Settings = () => {
           </div>
           <>{MemoizedImages}</>
         </div>
-        <div className="w-full relative -ml-5 my-8">
-          <svg
-            viewBox="0 0 1024 1024"
-            height={30}
-            width={30}
-            version="1.1"
-            className="icon absolute right-0 cursor-pointer"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="#000000"
-            onClick={() => {
-              formikobj.setFieldValue('details', [
-                ...formikobj.values.details,
-                { from: '', to: '', price: 0 },
-              ]);
-            }}
-          >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              <path
-                d="M512 1024C229.7 1024 0 794.3 0 512S229.7 0 512 0s512 229.7 512 512-229.7 512-512 512z m0-938.7C276.7 85.3 85.3 276.7 85.3 512S276.7 938.7 512 938.7 938.7 747.3 938.7 512 747.3 85.3 512 85.3z"
-                fill="#00ff2a"
-              ></path>
-              <path
-                d="M682.7 554.7H341.3c-23.6 0-42.7-19.1-42.7-42.7s19.1-42.7 42.7-42.7h341.3c23.6 0 42.7 19.1 42.7 42.7s-19.1 42.7-42.6 42.7z"
-                fill="#00ff1e"
-              ></path>
-              <path
-                d="M512 725.3c-23.6 0-42.7-19.1-42.7-42.7V341.3c0-23.6 19.1-42.7 42.7-42.7s42.7 19.1 42.7 42.7v341.3c0 23.6-19.1 42.7-42.7 42.7z"
-                fill="#00ff1e"
-              ></path>
-            </g>
-          </svg>
-        </div>
+
         <button
           type="submit"
           disabled={!!isloading}
