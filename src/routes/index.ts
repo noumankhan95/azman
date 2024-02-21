f77c5f2aabfba0bd12871a1c7315787b47b97e64
Nouman <noumankhan95@yahoo.com>
2024-02-21 20:55:47 +0500

    removed boilerplate routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,8 @@
 import { lazy } from 'react';
 const UserContracts = lazy(() => import('../pages/User_Contracts'));
 
-const Calendar = lazy(() => import('../pages/Calendar'));
-const Chart = lazy(() => import('../pages/Chart'));
-const FormElements = lazy(() => import('../pages/Form/FormElements'));
-const FormLayout = lazy(() => import('../pages/Form/FormLayout'));
-const Profile = lazy(() => import('../pages/Profile'));
 const Settings = lazy(() => import('../pages/Settings'));
-const Tables = lazy(() => import('../pages/Tables'));
-const Alerts = lazy(() => import('../pages/UiElements/Alerts'));
-const Buttons = lazy(() => import('../pages/UiElements/Buttons'));
+
 const AddUsers = lazy(() => import('../pages/AddUsers'));
 const UserDetails = lazy(() => import('../pages/UserDetails'));
 const Contracts = lazy(() => import('../pages/Contracts'));
@@ -35,11 +28,7 @@ const coreRoutes = [
     title: 'Contracts',
     component: Contracts,
   },
-  {
-    path: '/calendar',
-    title: 'Calender',
-    component: Calendar,
-  },
+
   {
     path: '/userDetails',
     title: 'User Details',
@@ -51,46 +40,13 @@ const coreRoutes = [
     title: 'Staff',
     component: AddUsers,
   },
-  {
-    path: '/profile',
-    title: 'Profile',
-    component: Profile,
-  },
-  {
-    path: '/forms/form-elements',
-    title: 'Forms Elements',
-    component: FormElements,
-  },
-  {
-    path: '/forms/form-layout',
-    title: 'Form Layouts',
-    component: FormLayout,
-  },
-  {
-    path: '/tables',
-    title: 'Tables',
-    component: Tables,
-  },
+
   {
     path: '/settings',
     title: 'Settings',
     component: Settings,
   },
-  {
-    path: '/chart',
-    title: 'Chart',
-    component: Chart,
-  },
-  {
-    path: '/ui/alerts',
-    title: 'Alerts',
-    component: Alerts,
-  },
-  {
-    path: '/ui/buttons',
-    title: 'Buttons',
-    component: Buttons,
-  },
+
   {
     path: '/userContracts',
     title: 'User Contracts',
